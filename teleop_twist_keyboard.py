2a33ff24dcb677638ac97150e802829f1495517c
Michael Lu <lu.michael@outlook.com>
2020-09-24 14:26:15 -0700

    refactor to use jetcar messages

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -1,82 +1,55 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import tty
+import termios
+import select
+import sys
+import rospy
 
 import threading
 
-import roslib; roslib.load_manifest('teleop_twist_keyboard')
-import rospy
+import math
+from pub.msg import jetRacerCar as JetRacerCarMsg
 
-from geometry_msgs.msg import Twist
 
-import sys, select, termios, tty
-
+BREAK_KEY = ' '
 msg = """
-Reading from the keyboard  and Publishing to Twist!
+Reading from the keyboard  and Publishing to /jetRacer_Controller!
 ---------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+Increase throttle by 1: w
+Decrease throttle by -1: s
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
+Increase steering angle left by pi/96: a 
+Decrease steering angle right by pi/96: d 
 
-t : up (+z)
-b : down (-z)
+Break: space
 
-anything else : stop
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
+Fine tune steering angle
+Increase steering angle left by pi/192: A 
+Decrease steering angle right by pi/192: D 
 
 CTRL-C to quit
 """
 
-moveBindings = {
-        'i':(1,0,0,0),
-        'o':(1,0,0,-1),
-        'j':(0,0,0,1),
-        'l':(0,0,0,-1),
-        'u':(1,0,0,1),
-        ',':(-1,0,0,0),
-        '.':(-1,0,0,1),
-        'm':(-1,0,0,-1),
-        'O':(1,-1,0,0),
-        'I':(1,0,0,0),
-        'J':(0,1,0,0),
-        'L':(0,-1,0,0),
-        'U':(1,1,0,0),
-        '<':(-1,0,0,0),
-        '>':(-1,-1,0,0),
-        'M':(-1,1,0,0),
-        't':(0,0,1,0),
-        'b':(0,0,-1,0),
-    }
+throttleBindings = {
+    'w': 1,
+    's': -1,
+}
 
-speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
-    }
+steeringBindings = {
+    'a': math.pi / 96,
+    'd': -math.pi / 96,
+    'A': math.pi / 192,
+    'D': -math.pi / 192
+}
 
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
-        self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.th = 0.0
-        self.speed = 0.0
-        self.turn = 0.0
+        self.publisher = rospy.Publisher('/jetRacer_Controller', JetRacerCarMsg, queue_size=1)
+        self.throttle = 0.0
+        self.steerAngle = 0.0
         self.condition = threading.Condition()
         self.done = False
 
@@ -93,58 +66,47 @@ class PublishThread(threading.Thread):
         i = 0
         while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
             if i == 4:
-                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+                print("Waiting for subscriber to connect to {}".format(
+                    self.publisher.name))
             rospy.sleep(0.5)
             i += 1
             i = i % 5
         if rospy.is_shutdown():
-            raise Exception("Got shutdown request before subscribers connected")
+            raise Exception(
+                "Got shutdown request before subscribers connected")
 
-    def update(self, x, y, z, th, speed, turn):
+    def update(self, throttle, steerAngle):
         self.condition.acquire()
-        self.x = x
-        self.y = y
-        self.z = z
-        self.th = th
-        self.speed = speed
-        self.turn = turn
+        self.throttle = throttle
+        self.steerAngle = steerAngle
         # Notify publish thread that we have a new message.
         self.condition.notify()
         self.condition.release()
 
     def stop(self):
         self.done = True
-        self.update(0, 0, 0, 0, 0, 0)
+        self.update(0, 0)
         self.join()
 
     def run(self):
-        twist = Twist()
+        msg = JetRacerCarMsg()
         while not self.done:
             self.condition.acquire()
             # Wait for a new message or timeout.
             self.condition.wait(self.timeout)
 
-            # Copy state into twist message.
-            twist.linear.x = self.x * self.speed
-            twist.linear.y = self.y * self.speed
-            twist.linear.z = self.z * self.speed
-            twist.angular.x = 0
-            twist.angular.y = 0
-            twist.angular.z = self.th * self.turn
+            msg.steerAngle = self.steerAngle
+            msg.throttle = self.throttle
 
             self.condition.release()
 
             # Publish.
-            self.publisher.publish(twist)
+            self.publisher.publish(msg)
 
         # Publish stop message when thread exits.
-        twist.linear.x = 0
-        twist.linear.y = 0
-        twist.linear.z = 0
-        twist.angular.x = 0
-        twist.angular.y = 0
-        twist.angular.z = 0
-        self.publisher.publish(twist)
+        msg.steerAngle = 0
+        msg.throttle = 0
+        self.publisher.publish(msg)
 
 
 def getKey(key_timeout):
@@ -158,16 +120,17 @@ def getKey(key_timeout):
     return key
 
 
-def vels(speed, turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
+def vels(throttle, steerAngle):
+    return "currently:\tthrottle %s\tsteerAngle %s " % (throttle, steerAngle)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     settings = termios.tcgetattr(sys.stdin)
 
     rospy.init_node('teleop_twist_keyboard')
 
-    speed = rospy.get_param("~speed", 0.5)
-    turn = rospy.get_param("~turn", 1.0)
+    throttle = rospy.get_param("~throttle", 0.0)
+    steerAngle = rospy.get_param("~steerAngle", 0.0)
     repeat = rospy.get_param("~repeat_rate", 0.0)
     key_timeout = rospy.get_param("~key_timeout", 0.0)
     if key_timeout == 0.0:
@@ -175,34 +138,43 @@ if __name__=="__main__":
 
     pub_thread = PublishThread(repeat)
 
-    x = 0
-    y = 0
-    z = 0
-    th = 0
     status = 0
-
     try:
         pub_thread.wait_for_subscribers()
-        pub_thread.update(x, y, z, th, speed, turn)
+        pub_thread.update(throttle, steerAngle)
 
         print(msg)
-        print(vels(speed,turn))
+        print(vels(throttle, steerAngle))
         while(1):
             key = getKey(key_timeout)
-            if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = speed * speedBindings[key][0]
-                turn = turn * speedBindings[key][1]
+            if key == BREAK_KEY:
+                print("BREAK")
+                throttle = 0.0
+                steerAngle = 0.0
+            elif key in throttleBindings.keys():
+                throttle += throttleBindings[key]
+                throttle = min(throttle, 100)
+                # prevent car from going backwards
+                throttle = max(throttle, 0)
 
-                print(vels(speed,turn))
+                print(vels(throttle, steerAngle))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+
+            elif key in steeringBindings.keys():
+                steerAngle += steeringBindings[key]
+
+                # keep steerAngle in range
+                steerAngle = max(steerAngle, -math.pi/12)
+                steerAngle = min(steerAngle, math.pi/12)
+
+                print(vels(throttle, steerAngle))
                 if (status == 14):
                     print(msg)
                 status = (status + 1) % 15
             else:
+                '''
                 # Skip updating cmd_vel if key timeout and robot already
                 # stopped.
                 if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
@@ -211,10 +183,12 @@ if __name__=="__main__":
                 y = 0
                 z = 0
                 th = 0
+                '''
                 if (key == '\x03'):
+                    pub_thread.stop()
                     break
- 
-            pub_thread.update(x, y, z, th, speed, turn)
+
+            pub_thread.update(throttle, steerAngle)
 
     except Exception as e:
         print(e)
